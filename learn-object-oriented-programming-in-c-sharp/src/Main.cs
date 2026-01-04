64b378665ba598d3f1f2d383ad697609348d485f
Mohammad Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2026-01-04 14:47:40 +0600

    learn about the basic structure of C# Program

--- learn-object-oriented-programming-in-c-sharp/src/Main.cs
+++ learn-object-oriented-programming-in-c-sharp/src/Main.cs
@@ -1,1 +1,11 @@
-Console.WriteLine("Hello World!");
+using System;
+using learn_object_oriented_programming_in_c_sharp;
+
+class Program
+{
+  public static void Main(string[] args)
+  {
+    ProgramStructure programStructure = new ProgramStructure();
+    programStructure.Run();
+  }
+}
---
+++ learn-object-oriented-programming-in-c-sharp/src/ProgramStructure.cs
@@ -0,0 +1,37 @@
+using System;
+namespace learn_object_oriented_programming_in_c_sharp
+{
+  public class ProgramStructure
+  {
+    public void Run()
+    {
+      Console.WriteLine("\n ================================= Basic Structure Of C# Program Structure =================================");
+      /*
+        * using System --> System is a built-in namespace is C#.Inside System, it contains Console,Math,String,Exception
+       * 
+       * We are using Console.WriteLine() to print in console if we don't use system we Have to write code like this way
+       * System.Console.WriteLine("================================= Hello World =================================");
+        
+       * namespace learn_object_oriented_programming_in_c_sharp --> namespace is logical folder.
+          * 1. Inside Big Project we have multiple classes
+          * 2. To maintain Class name Different namespace will help us 
+        
+        * class --> class is a user defined blueprint with some attributes and behaviours
+          * 1. class is not allocated memory when it is defined
+        
+       * static void Main(string[] args)
+       * Main Method --> Main method is the entry point of the C#,Java,C++ or any Programing language
+
+       * static --> static allows us to call the static variable,methods whatever it is without creating an object
+
+       * void --> void is the return type.void means this method/function don't return anything
+        
+       * (string[] args) --> it's a command line argument
+        
+       * ; --> it means it's the end of the line just like fullstop(.) in English
+        
+       * Console.WriteLine(""); --> It help us to print message or instruction in console 
+       */
+    }
+  }
+}
